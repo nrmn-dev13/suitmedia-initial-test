d9e722ba8b41a0d38ffc9b4479bb80ca3853fef0
Norman <normanramadhan3@gmail.com>
2024-02-28 06:47:59 +0700

    setup theme color

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div class="container mx-auto">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
     <HelloWorld msg="Vite + Vue" />
+    <h1 class="text-blue">Test</h1>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
+    colors: {
+      'black': '#000000',
+      'blue': '#003459',
+      'dark-blue': '#242B33',
+      'gray': '#667479',
+      'soft-gray': '#99A2A5',
+      'yellow': '#FCEED5',
+      'secondary-yellow': '#F7DBA7',
+      'dark-yellow': '#FFB775',
+    },
   },
   plugins: [],
 }
-
