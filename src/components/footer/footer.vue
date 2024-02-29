9d26e7ccfedfc95b77bccb595764686231ac8b7f
Norman <normanramadhan3@gmail.com>
2024-02-29 07:19:50 +0700

    adjust footer

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -14,36 +14,36 @@ import Logo from '../../assets/logo.svg'
       <nav class="footer-nav">
         <ul class="primary-nav">
           <li>
-            <a href="" class="link">Home</a>
+            <a href="" class="link text-blue">Home</a>
           </li>
           <li>
-            <a href="" class="link">Category</a>
+            <a href="" class="link text-blue">Category</a>
           </li>
           <li>
-            <a href="" class="link">About</a>
+            <a href="" class="link text-blue">About</a>
           </li>
           <li>
-            <a href="" class="link">Contact</a>
+            <a href="" class="link text-blue">Contact</a>
           </li>
         </ul>
         <ul class="social-nav">
           <li>
-            <a href="" class="link">
+            <a href="" class="link social--link">
               <font-awesome-icon :icon="['fab', 'facebook']" />
             </a>
           </li>
           <li>
-            <a href="" class="link">
+            <a href="" class="link social--link">
               <font-awesome-icon :icon="['fab', 'twitter']" />
             </a>
           </li>
           <li>
-            <a href="" class="link">
+            <a href="" class="link social--link">
               <font-awesome-icon :icon="['fab', 'instagram']" />
             </a>
           </li>
           <li>
-            <a href="" class="link">
+            <a href="" class="link social--link">
               <font-awesome-icon :icon="['fab', 'youtube']" />
             </a>
           </li>
@@ -56,11 +56,11 @@ import Logo from '../../assets/logo.svg'
           </a>
         </li>
         <li>
-          <a href="" class="link">© 2022 Monito. All rights reserved.</a>
+          <a href="" class="link copyright--link">© 2022 Monito. All rights reserved.</a>
         </li>
         <li>
-          <a href="" class="link">Terms of Service</a>
-          <a href="" class="link">Privacy Policy</a>
+          <a href="" class="link copyright--link">Terms of Service</a>
+          <a href="" class="link copyright--link">Privacy Policy</a>
         </li>
       </ul>
     </section>
@@ -78,6 +78,17 @@ import Logo from '../../assets/logo.svg'
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding-bottom: 40px;
+    margin-bottom: 40px;
+    position: relative;
+    &::after {
+      content: '';
+      position: absolute;
+      border-bottom: 1px solid #CCD1D2;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
   }
 
   .primary-nav {
@@ -88,11 +99,28 @@ import Logo from '../../assets/logo.svg'
   .social-nav {
     gap: 40px;
     display: flex;
+    .social--link {
+      color: #003459;
+    }
   }
 
   ul.footer--bottom {
     display: flex;
     justify-content: space-between;
+    padding-bottom: 40px;
+    position: relative;
+
+    li {
+      &:first-child {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -100%);
+      }
+    }
+    .copyright--link {
+      color: #667479;
+    }
   }
 
   .section__footer {
@@ -106,6 +134,7 @@ import Logo from '../../assets/logo.svg'
     display: flex;
     gap: 20px;
     align-items: center;
+    margin: 60px 0 40px;
 
     .title {
       font-size: 24px;
@@ -130,6 +159,8 @@ import Logo from '../../assets/logo.svg'
         border: 1px solid #99A2A5;
         border-radius: 8px;
         flex: 1 1 auto;
+        padding: 14px 28px;
+        color: #99A2A5;
       }
     }
   }
